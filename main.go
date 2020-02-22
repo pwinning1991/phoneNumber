70702f513759aa9c009f6bc69fd4691488f4a3c1
Philip Winnington <ftcpjw@yahoo.com>
2020-02-22 10:03:17 -0500

    updating tests

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
+import "bytes"
+
 func normalize(phone string) string {
 	var buf bytes.Buffer
 
 	for _, ch := range phone {
 		if ch >= '0' && ch <= '9' {
-			buf.WriteRun(ch)
+			buf.WriteRune(ch)
 		}
 	}
 	return buf.String()
