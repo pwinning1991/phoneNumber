25675a1a74b3e45860f527056feaa4c4bbc82ef6
Philip Winnington <ftcpjw@yahoo.com>
2020-02-23 10:16:50 -0500

    adding query code for all phone numbers stored in db

--- main.go
+++ main.go
@@ -32,6 +32,15 @@ func main() {
 	id, err := insertPhone(db, "1234567890")
 	must(err)
 	fmt.Println("id=", id)
+	number, err := getPhone(db, id)
+	must(err)
+	fmt.Println(number)
+
+	phones, err := allPhones(db)
+	must(err)
+	for _, p := range phones {
+		fmt.Printf("%+v\n", p)
+	}
 }
 
 func createTable(db *sql.DB) error {
@@ -43,6 +52,40 @@ func createTable(db *sql.DB) error {
 
 	_, err := db.Exec(statement)
 	return err
+}
+
+type phone struct {
+	id     int
+	number string
+}
+
+func allPhones(db *sql.DB) ([]phone, error) {
+	rows, err := db.Query("SELECT id,value FROM phone_numbers")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var ret []phone
+	for rows.Next() {
+		var p phone
+		if err := rows.Scan(&p.id, &p.number); err != nil {
+			return nil, err
+		}
+		ret = append(ret, p)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func getPhone(db *sql.DB, id int) (string, error) {
+	var number string
+	err := db.QueryRow("SELECT value FROM phone_numbers WHERE id=$1", id).Scan(&number)
+	if err != nil {
+		return "", err
+	}
+	return number, nil
 }
 
 func insertPhone(db *sql.DB, phone string) (int, error) {
