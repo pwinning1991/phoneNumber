eb4aa439af51a8b203f8424b7e67c17dadd60fae
Philip Winnington <ftcpjw@yahoo.com>
2020-02-23 07:29:33 -0500

    added function to insert phone number

--- main.go
+++ main.go
@@ -19,16 +19,41 @@ var (
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	//db, err := sql.Open("postgres", psqlInfo)
+	//must(err)
+	//err = resetDB(db, dbname)
+	//must(err)
+	//db.Close()
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	must(err)
-	err = resetDB(db, dbname)
-	must(err)
-	db.Close()
-	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-	db, err = sql.Open("postgres", psqlInfo)
-	must(err)
 	defer db.Close()
-	must(db.Ping())
+	must(createTable(db))
+	id, err := insertPhone(db, "1234567890")
+	must(err)
+	fmt.Println("id=", id)
+}
+
+func createTable(db *sql.DB) error {
+	statement := `
+	CREATE TABLE IF NOT EXISTS phone_numbers (
+		id SERIAL,
+		value VARCHAR(255)
+	)`
+
+	_, err := db.Exec(statement)
+	return err
+}
+
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+
 }
 
 func must(err error) {
